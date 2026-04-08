0f79e60f5a50cd712efca6d79a2844185ffded0a
aderi2099-ai <aderi2099@gmail.com>
2026-04-08 17:14:54 +0800

    Update layout.tsx

--- layout.tsx
+++ layout.tsx
@@ -1,1 +1,13 @@
-
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="zh">
+      <body style={{ margin: 0, background: 'black', color: 'white' }}>
+        {children}
+      </body>
+    </html>
+  )
+}
